3b414bee100bf6e99b6dac7c920c95ea23d2854a
JM29R <juanmanuelrios5602@gmail.com>
2026-09-23 11:34:42 -0300

    Add backend Spring Boot

---
+++ Back-End/MediFlow/src/main/java/G10/EQUIPO13/MediFlow/MediFlowApplication.java
@@ -0,0 +1,13 @@
+package G10.EQUIPO13.MediFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediFlowApplication.class, args);
+	}
+
+}
